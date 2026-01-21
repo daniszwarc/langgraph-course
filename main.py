fd943386d9ae379f354e2edeee0d47f4f9210acb
Dani Szwarc <dani@thiez.com>
2026-01-21 18:15:05 -0500

    Add initial implementation of react.py and update main.py greeting message

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    print("Hello ReAct LangGraph with function calling")
+    print("Hello ReAct LangGraph with Function Calling")
